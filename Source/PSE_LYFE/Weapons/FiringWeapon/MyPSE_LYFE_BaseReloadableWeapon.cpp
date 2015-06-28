858ebb652956522e4d72eb2910d5d51977118fd5
Envenger <envenger@gmail.com>
2015-06-28 19:19:10 +0530

    Character Asset Renaming
    Renamed Male/ Female character assets and their locations

---
+++ Source/PSE_LYFE/Weapons/FiringWeapon/MyPSE_LYFE_BaseReloadableWeapon.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PSE_LYFE.h"
+#include "MyPSE_LYFE_BaseReloadableWeapon.h"
+
+
+
+
---
+++ Source/PSE_LYFE/Weapons/FiringWeapon/MyPSE_LYFE_BaseReloadableWeapon.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Weapons/FiringWeapon/PSE_LYFE_BaseFiringWeapon.h"
+#include "MyPSE_LYFE_BaseReloadableWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PSE_LYFE_API AMyPSE_LYFE_BaseReloadableWeapon : public APSE_LYFE_BaseFiringWeapon
+{
+	GENERATED_BODY()
+	
+/*
+	UPROPERTY(EditDefaultsOnly, Category = Ammo)
+	float ReloadingTime;
+
+	UPROPERTY(Transient, Replicated)
+	uint16 NoOfClips;
+
+	UPROPERTY(Transient, Replicated)
+	int32 CurrentAmmo;
+
+	UPROPERTY(EditDefaultsOnly, Category = Ammo)
+	int32 AmmoPerClip;
+	*/
+	
+};
